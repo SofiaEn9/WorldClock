e9b3ee360296e82df429bce81efa0d77b65c5cba
SofiaEn9 <sofiaanen9@gmail.com>
2023-07-28 11:21:56 -0600

    Remove the am/pm element to main city

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@ function updateTime() {
   let chihuahuaElement = document.querySelector("#chihuahua");
   let chihuahuaDateElement = chihuahuaElement.querySelector(".main-date");
   let chihuahuaTimeElement = chihuahuaElement.querySelector(".main-time");
-  let chihuahuaAmElement = chihuahuaElement.querySelector(".main-am-pm");
   let chihuahuaTime = moment().tz("America/Chihuahua");
 
   chihuahuaDateElement.innerHTML = chihuahuaTime.format("dddd, MMMM Do");
   chihuahuaTimeElement.innerHTML = chihuahuaTime.format("HH:mm");
-  chihuahuaAmElement.innerHTML = chihuahuaTime.format("A");
-  console.log(chihuahuaElement);
 }
 
 updateTime();
