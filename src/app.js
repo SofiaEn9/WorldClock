97203f202376bcd59663411db9ec119de0637844
SofiaEn9 <sofiaanen9@gmail.com>
2023-07-28 12:06:19 -0600

    Add functionality to search bar

--- src/app.js
+++ src/app.js
@@ -8,5 +8,22 @@ function updateTime() {
   chihuahuaTimeElement.innerHTML = chihuahuaTime.format("HH:mm");
 }
 
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  cityName = cityTimezone.replace("_", " ").split("/")[1];
+  cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="searchedCity">
+    <div class="city">${cityName}</div>
+    <div class="time">${cityTime.format("HH:mm")}</div>
+    <div class="date">${cityTime.format("dddd, MMMM Do")}</div>
+  </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
